53cbaf8dcb6d4d2d296d412c5c4c19cbb53ccd4e
mel <luminousbdaj@gmail.com>
2026-06-06 01:49:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 from telegram import ReplyKeyboardMarkup, Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ContextTypes
+)
 
 TOKEN = os.getenv("7997357489:AAFdRMLu58TBx2CeFzmZtsf_a7t0iuYsbwE")
 
@@ -14,7 +20,7 @@ menu = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "приветусики\nя рядом. выбери кнопку:",
+        "приветусики 🌙\nя рядом. выбери кнопку:",
         reply_markup=menu
     )
 
@@ -22,31 +28,50 @@ async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
     if text == "Настроение":
-        await update.message.reply_text("как ты себя чувствуешь?")
+        await update.message.reply_text(
+            "как ты себя чувствуешь?"
+        )
 
     elif text == "Сон":
-        await update.message.reply_text("что снилось?")
+        await update.message.reply_text(
+            "что снилось?"
+        )
 
     elif text == "Таблетки":
-        await update.message.reply_text("ты выпила таблетки?")
+        await update.message.reply_text(
+            "ты выпила таблетки?"
+        )
 
     elif text == "Музыка":
-        await update.message.reply_text("что сегодня?")
+        await update.message.reply_text(
+            "что сегодня?"
+        )
 
     elif text == "Поговорить":
-        await update.message.reply_text("что такое пупсеночек?")
+        await update.message.reply_text(
+            "что такое пупсеночек?"
+        )
 
     elif text == "Мини-игры":
-        await update.message.reply_text("пока не готово")
+        await update.message.reply_text(
+            "пока не готово"
+        )
 
     else:
-        await update.message.reply_text("хорошо")
+        await update.message.reply_text(
+            "хорошо"
+        )
 
-print("бот запущен")
+print("бот запущен 🌙")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message))
+app.add_handler(
+    MessageHandler(
+        filters.TEXT & ~filters.COMMAND,
+        message
+    )
+)
 
 app.run_polling()
